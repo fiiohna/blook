a5d7e96b2ad3b78405951135a51071e8be9bc867
Justina Chua <111348212+justinachuayi@users.noreply.github.com>
2023-04-02 12:37:16 +0800

    Delete PaymentSuccess.jsx

--- src/pages/PaymentSuccess.jsx
+++
@@ -1,7 +0,0 @@
-import React, {useState} from "react";
-import PaymentSuccess from "../components/payment/paymentstatus";
-export default function PaymentSuccess(){
-    return(
-        <PaymentSuccess></PaymentSuccess>
-    )
-}
