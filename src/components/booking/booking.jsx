2d36e2916ae8ed821a8693a54efeddeb163c3855
Justina Chua <111348212+justinachuayi@users.noreply.github.com>
2023-04-02 15:14:55 +0800

    .

--- src/components/booking/booking.jsx
+++ src/components/booking/booking.jsx
@@ -6,11 +6,17 @@ export default function Booking({ activity }) {
     const { fees } = activity
 
     return (
+        
         <div className='booking'>
-            Booking Price
-            {fees}
-
-            <Form>
+            <div className = "booking__top d-flex align-items-scenter justify-content-between">
+                <h3>Booking Price</h3>
+                {fees}
+            </div>
+           
+            <div className = "booking__form">
+                <h5>Information</h5>
+            
+            <Form className = "booking__info-form">
                 <Form.Group className="mb-3" id="fullName">
                     <Form.Label>Full Name</Form.Label>
                     <Form.Control type="text" placeholder="Enter your full name" required/>
@@ -23,7 +29,7 @@ export default function Booking({ activity }) {
 
                 <Form.Group className="mb-3" id="phone">
                     <Form.Label>Phone Number</Form.Label>
-                    <Form.Control type="number" placeholder="Enter phone number" />
+                    <Form.Control type="text" placeholder="Enter phone number" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" id="c_address">
@@ -35,6 +41,7 @@ export default function Booking({ activity }) {
                     <Form.Control type="text" placeholder="Enter your billing address" />
                 </Form.Group>
             </Form>
+            </div>
         </div>
     )
 }
--- src/pages/ActivityDetail.jsx
+++ src/pages/ActivityDetail.jsx
@@ -3,6 +3,10 @@ import { useParams } from 'react-router-dom';
 import activities from "../assets/data/activities"
 import { Container, Row, Col, Form, ListGroup } from 'react-bootstrap';
 import Booking from '../components/booking/booking';
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 
 import { HiOutlineLocationMarker } from 'react-icons/hi'
 
@@ -15,12 +19,14 @@ export default function ActivityDetail() {
     // destructure properties from activity object
     const { imgSrc, name, address, price, description } = activity
 
+    // const {totalRating, avgRating} = calculateAvgRating
+
     // This component will display the details of the activity
     return (
         <section style={{ paddingTop: "100px" }}>
             <Container>
                 <Row>
-                    <Col lg={8}>
+                    <Col lg= '8'>
                         <div className="activitityContent">
                             <img src={imgSrc} alt="image" />
 
@@ -43,20 +49,44 @@ export default function ActivityDetail() {
                         </div>
                     </Col>
 
-                    <Col lg={4}>
+                    <Col lg= '6'>
                         <Booking activity={activity}></Booking>
                     </Col>
                 </Row>
             </Container>
         </section>
-        // <div className= "booking">
-        //     <div className = "booking__top d-flex align-items-center justify-content-between">
-        //         <h3>(price) <span>/per person</span></h3>
-        //         <span className = "tour__rating d-flex align-items-center">
-        //             <i class = "ri-star-s-fill"></i>
-        //             average rating (reviews length)
-        //         </span>
-        //     </div>
-        // </div>
+        
+        // <section style={{ paddingTop: "100px" }}>
+        //     <Container>
+        //          <Row>
+        //              <Col lg={8}>
+        //                  <div className="activitityContent">
+        //                      <img src={imgSrc} alt="image" />
+
+        //                     <div className="activityInfo">
+        //                         <h4 className="name">{name}</h4>
+        //                         <span className="continent flex"><HiOutlineLocationMarker className='icon' /></span>
+        //                         <span className="name">{address}</span>
+
+        //                         <div className="price flex">
+        //                                  <h3>SGD{price}</h3>
+        //                             </div>
+        //                         </div>
+
+        //                        <div className="desc">
+        //                             <p>{description}</p>
+        //                         </div>
+
+        //                      </div>
+        //                 </div>
+        //              </Col>
+
+        //             <Col lg={4}>
+        //                  <Booking activity={activity}></Booking>
+        //              </Col>
+        //          </Row>
+        //      </Container>
+        // </section>
+        
     );
 };
