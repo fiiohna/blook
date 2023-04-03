d0eb32c36163a9526b9e76a784db5619d9ff1d23
Justina Chua <111348212+justinachuayi@users.noreply.github.com>
2023-04-03 22:16:31 +0800

    Update vendorverify.jsx

--- src/components/vendorverify/vendorverify.jsx
+++ src/components/vendorverify/vendorverify.jsx
@@ -3,14 +3,14 @@ import React from "react";
 export default function VendorVerify() {
     return (
         
-            <div style = {{paddingTop: "100px"}}>
-                <b style = {{fontsize: "500px"}}>Verify Booking</b>
-      <textarea
-        style={{ height: '400px', width: '100%' }}
+            <div style = {{padding: "100px", textAlign: "center"}} >
+                <b style = {{fontSize: "100px"}}>Verify Booking</b>
+      <input 
+        type = "number" style={{ height: '400px', width: '100%', fontSize: "200px"}}
         // value={text}
         // onChange={(e) => setText(e.target.value)}
       />
-      <button className='btn bookingbtn'>Submit</button>
+      <button className='btn bookingbtn' style = {{fontSize: "50px"}}>Submit</button>
     </div>
        
     )
