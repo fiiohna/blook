8bb110fd549bad7f0c3de1d0dc22aefa650c2209
lionelgwk <lionelgwk@icloud.com>
2023-04-03 18:08:09 +0800

    conflict resolved

--- src/components/recommendation/recommendation.jsx
+++ src/components/recommendation/recommendation.jsx
@@ -63,10 +63,7 @@ export default function Recommendation() {
 
     const handleBook = (e) => {
         localStorage.setItem("activityBookId", e.target.value);
-<<<<<<< HEAD
         navigate("/activitydetail");
-=======
->>>>>>> 58624c74bbd146b7a3742f2ae26814e1c5e60394
         // console.log(e.target.value);
     };
 
